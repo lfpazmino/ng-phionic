42ee246a24364e75b1830527e73139d7eafd81c3
Luis Fernando Pazmino <lfpazmino@gmail.com>
2014-04-23 23:42:24 -0300

    Sample branding

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,7 +3,7 @@
 describe('Controller: MainController', function () {
 
   // load the controller's module
-  beforeEach(module('codexApp'));
+  beforeEach(module('sampleApp'));
 
   var MainCtrl,
     scope;
